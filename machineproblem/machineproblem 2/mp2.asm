7d46f8a7bef81325d82a51b0cb102b463a7d14c5
Marinay Wilfredo Genabe Jr <114600451+derf567@users.noreply.github.com>
2024-03-12 10:08:08 +0800

    Add files via upload

--- machineproblem/machineproblem 2/mp2.asm
+++ machineproblem/machineproblem 2/mp2.asm
@@ -1,1 +1,44 @@
-; Create an ascii art. see https://www.asciiart.eu/
+section .data
+    newline db 10
+
+section .text
+    global _start
+
+_start:
+    ; Print ASCII art
+    mov   eax, 4      ; sys_write syscall
+    mov   ebx, 1      ; stdout fileno
+
+    ; ASCII art pattern
+    mov   ecx, art_pattern
+    mov   edx, art_size
+    int   80h         ; sys_write(1, art_pattern, art_size)
+
+    ; Print newline
+    mov   eax, 4
+    mov   ebx, 1
+    mov   ecx, newline
+    mov   edx, 1
+    int   80h
+
+    ; System exit
+    mov eax, 1
+    mov ebx, 0
+    int 80h
+
+section .data
+    art_pattern db '  *  ', 10
+                db ' *** ', 10
+                db '*****', 10
+                db ' *** ', 10
+                db '  *  ', 0
+    art_size equ $ - art_pattern
+
+
+
+
+;sudo apt-get update
+;sudo apt-get -y install nasm
+;nasm -f elf mp2.asm
+;ld -m  elf_i386 mp2.o -o mp2
+;./mp2
