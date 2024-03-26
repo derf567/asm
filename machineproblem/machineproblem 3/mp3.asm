77b66910272113fbdd897692c1372c900b9ed88f
Marinay Wilfredo Genabe Jr <114600451+derf567@users.noreply.github.com>
2024-03-26 10:52:51 +0800

    Add files via upload

--- machineproblem/machineproblem 3/mp3.asm
+++ machineproblem/machineproblem 3/mp3.asm
@@ -1,109 +1,44 @@
+section .bss
+    buffer resb 1
+
 section .data
-    newline db 10  ; ASCII code for newline character
+    newline db 10      
 
 section .text
     global _start
 
-    ; Function to print a character
-    print_char:
-        mov eax, 4       ; System call number for write
-        mov ebx, 1       ; File descriptor (stdout)
-        mov ecx, esp     ; Pointer to the character on the stack
-        mov edx, 1       ; Number of bytes to write
-        int 0x80         ; Software interrupt to invoke system call
-        ret
+_start:
+    mov eax, 3           
+    mov ebx, 0        
+    mov ecx, buffer
+    mov edx, 1         
+    int 0x80        
 
-    _start:
-
-        ; Read character from memory (replace with your character input method)
-        ; Assuming the character is in AL register after the input
-
-        ; Check if character is uppercase (between 'A' and 'Z')
-        cmp al, 'A'
-        jl not_uppercase  ; Jump if less than 'A' (not uppercase)
-        cmp al, 'Z'
-        jg not_uppercase  ; Jump if greater than 'Z' (not uppercase)
-
-        ; Convert to lowercase (add 32 to ASCII code)
-        add al, 32
-
-    not_uppercase:
-
-        ; Print the character
-        push eax          
-        call print_char  
-
-        ; Print line feed (optional)
-        mov eax, 4        
-        mov ebx, 1        
-        mov ecx, newline  
-        mov edx, 1        
-        int 0x80         
-
-    ; Original printing functionality (unmodified)
-
-        mov eax, 4       
-        mov ebx, 1       
-        mov ecx, msg1     
-        mov edx, 1       
-        int 0x80          
-
-        mov eax, 4        
-        mov ebx, 1        
-        mov ecx, newline  
-        mov edx, 1       
-        int 0x80        
-
-        mov eax, 4       
-        mov ebx, 1        
-        mov ecx, msg2    
-        mov edx, 1        
-        int 0x80          
-
-        mov eax, 4       
-        mov ebx, 1        
-        mov ecx, newline  
-        mov edx, 1        
-        int 0x80        
-
-        mov eax, 4    
-        mov ebx, 1       
-        mov ecx, msg3     
-        mov edx, 1        
-        int 0x80          
-
-        mov eax, 4        
-        mov ebx, 1       
-        mov ecx, newline  
-        mov edx, 1        
-        int 0x80          
-
-        mov eax, 4        
-        mov ebx, 1        
-        mov ecx, msg4     
-        mov edx, 1        
-        int 0x80          
-
-        mov eax, 4       
-        mov ebx, 1        
-        mov ecx, newline  
-        mov edx, 1        
-        int 0x80         
-
-    ; Exit program
-        mov eax, 1       
-        xor ebx, ebx      
-        int 0x80          
-
-    section .data
-        msg1 db 'a'       
-        msg2 db 'A'      
-        msg3 db 'z'     
-        msg4 db 'Z'      
+    cmp byte [buffer], 'a'
+    jl not_lowercase
+    cmp byte [buffer], 'z'
+    jg not_lowercase
 
 
-;sudo apt-get update
-;sudo apt-get -y install nasm
-;nasm -f elf mp3.asm
-;ld -m  elf_i386 mp3.o -o mp3
-;./mp3
+    sub byte [buffer], 32 
+
+not_lowercase:
+    ; Display the entered character
+    mov eax, 4         
+    mov ebx, 1           
+    mov ecx, buffer   
+    mov edx, 1       
+    int 0x80    
+
+
+    ; Display a new line
+    mov eax, 4           ; sys_write system call number
+    mov ebx, 1           ; file descriptor: stdout
+    mov ecx, newline     ; pointer to the newline character
+    mov edx, 1           ; length of the newline character
+    int 0x80             ; invoke system call        
+
+    ; Exit the program
+    mov eax, 1        
+    xor ebx, ebx      
+    int 0x80          
